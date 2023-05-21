bb3b394b05a5972eb908451fcb6d5ae0660f1663
James Ryan Justo <jrajusto@mymail.mapua.edu.ph>
2023-05-21 17:33:03 +0800

    update

--- speechRecog.py
+++ speechRecog.py
@@ -1,10 +1,12 @@
 import speech_recognition as sr
+import pyttsx3
 
 
 
 def readMicrophone():
     r = sr.Recognizer()
     with sr.Microphone() as source:
+        r.adjust_for_ambient_noise(source,duration=0.2)
         print("Please state your query:")
         audio = r.listen(source)
         try:
